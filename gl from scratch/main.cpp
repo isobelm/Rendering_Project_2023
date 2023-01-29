30b7ffed09dea610282ba893c3c551297fab9fab
isobelm <isobel.mahon@gmail.com>
2023-01-29 15:41:39 +0000

    Initial Commit

---
+++ gl from scratch/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  gl from scratch
+//
+//  Created by Isobel Mahon on 29/01/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
